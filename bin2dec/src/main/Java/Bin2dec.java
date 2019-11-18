a2a647fe1fdb7433498a4a78c08fa91c3b13e7a6
SUBRAMANIAN KARUNAKAREN <SUBBU.KARUNAKAREN@LibertyMutual.com>
2019-11-18 17:25:49 -0500

    updated for validateInput

--- bin2dec/src/main/Java/Bin2dec.java
+++ bin2dec/src/main/Java/Bin2dec.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
+
 public class Bin2dec {
 
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("printing");    }
+        translateBinary();
+
+    }
+
+    private static void translateBinary(){
+        String input;
+        int count = 0;
+        do{
+            System.out.println("Enter a binanry Number:");
+            input = scanner.nextLine();
+            System.out.println(input);
+            count = count + 1;
+            System.out.println("Count is -"+ count);
+        }
+        while (validateInput(input) && count < 8);
+    }
+
+    private static boolean validateInput(String stringInput){
+
+        boolean isValid;
+        Integer numInput = Integer.parseInt(stringInput);
+        if(numInput == 0 || numInput == 1)
+        {
+            isValid = true;
+        }
+        else{
+            System.out.println("You have entered a invalid number. ONLY Binary Numbers 0 or 1 are allowed. So Exiting!!!");
+            isValid = false;
+        }
+
+        return isValid;
+    }
+
+    private static boolean checkCount(int numDigit){
+        boolean isCountValid;
+        if(numDigit < 8)
+            isCountValid = true;
+        else
+            isCountValid = false;
+
+        return isCountValid;
+    }
+
+
 }
